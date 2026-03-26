bdcaf8aeca5e6f4dbad3d0b3cbd193871ce75383
KieranStewart <kieranmstewart@gmail.com>
2026-03-25 22:18:33 -0400

    Added testing endpoint

---
+++ evil_gnu_bash/test_endpoint/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, Response, request, jsonify
+
+app = Flask(__name__)
+
+str:debug = ""
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return Response('<h1>Welcome to Bored API</h1>\nThis is the debug screen, if you are seeing this in production then don\'t.<br><br>\n' + str(debug), 200)
+    elif request.method == 'POST':
+        debug += str(request.data)
+        return Response('Post recieved at default endpoint, added to debug', 200)
+
+
+if __name__ == "__main__":
+    app.run(port=8080, host="0.0.0.0")
