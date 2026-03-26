81a6ee0b78e85e2abae5995d0e6499531a89968b
KieranStewart <kieranmstewart@gmail.com>
2026-03-25 23:06:09 -0400

    fixed test api settings

--- evil_gnu_bash/test_endpoint/app.py
+++ evil_gnu_bash/test_endpoint/app.py
@@ -2,14 +2,23 @@ from flask import Flask, Response, request, jsonify
 
 app = Flask(__name__)
 
-debug = "Start:"
+debug = "<h2>Debug Start:</h2><br>"
+
+def get_debug():
+    global debug
+    return debug
+
+def append_debug(s):
+    global debug
+    debug += "<br>" + str(s)
+    return debug
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return Response('<h1>Welcome to Bored API</h1>\nThis is the debug screen, if you are seeing this in production then don\'t.<br><br>\n' + str(debug), 200)
+        return Response('<h1>Welcome to Bored API</h1>\nThis is the debug screen, if you are seeing this in production then don\'t.<br><br>\n' + get_debug(), 200)
     elif request.method == 'POST':
-        debug += str(request.data) + "<br>"
+        append_debug(request.get_json(force=True))
         return Response('Post recieved at default endpoint, added to debug', 200)
 
 
