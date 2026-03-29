ad882021c3609223b32df0f24280d94d235d1553
Kieran Stewart <kieranmstewart@gmail.com>
2026-03-28 23:03:25 -0400

    Updated test_endpoint

--- gnu_bosh/test_endpoint/app.py
+++ gnu_bosh/test_endpoint/app.py
@@ -2,25 +2,45 @@ from flask import Flask, Response, request, jsonify
 
 app = Flask(__name__)
 
-debug = "<h2>Debug Start:</h2><br>"
+import time
+filename = f"data_save_{str(time.time())}.json"
 
-def get_debug():
-    global debug
-    return debug
+data = []
 
-def append_debug(s):
-    global debug
-    debug += "<br>" + str(s)
-    return debug
+def get_formatted_data():
+    global data
+    if len(data) != 0:
+        out = "<tr>\n"
+        for key in data[0].keys():
+            out += f"\t<th>{key}</th>\n"
+        out += "</tr>\n"
+        for json in data:
+            out += "<tr>\n"
+            for key in json.keys():
+                out += f"\t<td>{json[key]}</td>\n"
+            out += "</tr>\n"
+    else:
+        return "No Data"
+            
+
+def add_data(s):
+    global data
+    data.append(s)
+    with open(filename, "a") as save:
+        save.write(f"\t{str(s)}\n")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return Response('<h1>Welcome to Bored API</h1>\nThis is the debug screen, if you are seeing this in production then don\'t.<br><br>\n' + get_debug(), 200)
+        return Response(f'<h1>Welcome to Red Team\'s Bashtrap Reciving Endpoint</h1>\nBoy I hope we replace this with something more official, works tho.<br>Here\'s what blue team is up to.<br>\n{get_formatted_data()}<br><br>ᓚᘏᗢ', 200)
     elif request.method == 'POST':
-        append_debug(request.get_json(force=True))
-        return Response('Post recieved at default endpoint, added to debug', 200)
+        add_data(request.get_json(force=True))
+        return Response('Post recieved at default endpoint, added to data', 200)
 
 
 if __name__ == "__main__":
+    with open(filename, "w") as save:
+        save.write("[\n")
     app.run(port=8080, host="0.0.0.0")
+    with open(filename, "a") as save:
+        save.write("\n]")
